4965b671a46a1b1d96cd655ecfce22d94dc00d81
Hurricane0 <nikitadv777@gmail.com>
2020-01-23 23:32:37 +0200

    Updates

---
+++ precache-manifest.b233180c80cf7c939dca7ab39989399c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcb3cb2e0d586c17497d4a70deb18fe",
+    "url": "/Fants-Game/index.html"
+  },
+  {
+    "revision": "1a63e6c55b31431dbd6a",
+    "url": "/Fants-Game/static/css/main.29f9e7d9.chunk.css"
+  },
+  {
+    "revision": "c330eac70355fcda4865",
+    "url": "/Fants-Game/static/js/2.cc236993.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Fants-Game/static/js/2.cc236993.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a63e6c55b31431dbd6a",
+    "url": "/Fants-Game/static/js/main.8ba64ed4.chunk.js"
+  },
+  {
+    "revision": "83326f0cbe3e02c1fb13",
+    "url": "/Fants-Game/static/js/runtime-main.853ca1ba.js"
+  },
+  {
+    "revision": "50e5cba2266ebd8f04d1b497c16b8040",
+    "url": "/Fants-Game/static/media/Infinity-1s-200px.50e5cba2.svg"
+  }
+]);
