7f2f501df00d1da7d7ad7087ec2e07a42f938d56
Hurricane0 <nikitadv777@gmail.com>
2020-03-13 22:28:12 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,103 +1,165 @@
-import React, {useState} from 'react';
-import {Time} from "./components/Time";
-import {Place} from "./components/Place";
-import {Action} from "./components/Action";
-import {Button} from "./components/Button";
-import {HashRouter, Route} from "react-router-dom";
-import {ModeButton} from "./components/ModeButton";
-import {Quests} from "./components/Quests";
+import React, { useState } from "react";
+import { Time } from "./components/Time";
+import { Place } from "./components/Place";
+import { Action } from "./components/Action";
+import { Button } from "./components/Button";
+import { HashRouter, Route } from "react-router-dom";
+import { ModeButton } from "./components/ModeButton";
+import { Quests } from "./components/Quests";
 
 function App() {
-    const [times,] = useState([
-        30, 60, 90, 120
-    ]);
-    const [places,] = useState([
-        "Губы Сн", "Шею", "Спину", "Грудь", "Живот", "Попу", "Губы Вн", "Вокруг", "Бедра", "Ступни", "Клитор", "Губы", "Клитор", "Клитор", "Клитор", "Клитор", "Клитор"
-    ]);
-    const [actions,] = useState([
-        "Лизать", "Сосать", "Кусать", "Целовать", "Гладить", "Щипать", "Засос", "Вставить", "Смотреть",
-    ]);
+  const [times] = useState([30, 60, 90, 120]);
+  const [places] = useState([
+    "Губы 💋",
+    "Шею",
+    "Спину",
+    "Грудь",
+    "Живот",
+    "Попу",
+    "Губы",
+    "Вокруг",
+    "Бедра",
+    "Ступни",
+    "Клитор",
+    "Губы"
+  ]);
+  const [actions] = useState([
+    "Лизать",
+    "Сосать",
+    "Кусать",
+    "Целовать",
+    "Гладить",
+    "Щипать",
+    "Засос",
+    "Вставить",
+    "Смотреть"
+  ]);
 
-    const [placesTwo,] = useState([
-        "Губы", "Шею", "Спину", "Грудь", "Живот", "Попу", "Губы", "Вокруг", "Бедра", "Плечи", "Член", "Яички"
-    ]);
-    const [actionsTwo,] = useState([
-        "Лизать", "Сосать", "Кусать", "Целовать", "Гладить", "Щипать", "Засос", "Вставить", "Смотреть"
-    ]);
-    const [quests,] = useState([
-        "Парень лежит на спине. Девушка медленно проводить грудью с ног до головы по парню, а в конце по очереди дает парню целовать два сосочка", "Парень полторы (две) минуты ласкает, целует ступни девушки", "Парень две минуты облизывает, кусает, целует попку девушки",
-        "Пара подходит к зеркалу и целуется на протяжении ~1 минуты", "Взять девушку двумя руками за грудь, и 10 раз проникнуть языком в вагину",
-        "Девушка на протяжении 30 секунд водит грудью по спине парня", "Девушка садится на стул и закидывает ноги на мужчину. Мужчина 2 минуты делает куни",
-        "Парень лежит на спине. Девушка медленно проводить грудью с ног до головы по парню, а в конце по очереди дает парню целовать два сосочка.",
-        "Парень ложится на спину. Девушка водит по лицу парня своей вагиной как захочет на протяжении 2 минут",
-        "Девушка целует парня от губ до члена на протяжении 1 минуты",
-        "Девушка прогибает спину и пытается полностью покласть грудь на кровать. Мужчина вводит палец или пальцы в девушку и минуту делает круговые движения внутри",
-        "Девушка ложится на край кровати. Парень садится на колени перед кроватью. максимально широко раздвигает ноги девушки в шпагат и делает куни на протяжении полторы минуты"
-    ])
+  const [placesTwo] = useState([
+    "Губы",
+    "Шею",
+    "Спину",
+    "Грудь",
+    "Живот",
+    "Попу",
+    "Губы",
+    "Вокруг",
+    "Бедра",
+    "Плечи",
+    "Член",
+    "Яички"
+  ]);
+  const [actionsTwo] = useState([
+    "Лизать",
+    "Сосать",
+    "Кусать",
+    "Целовать",
+    "Гладить",
+    "Щипать",
+    "Засос",
+    "Вставить",
+    "Смотреть"
+  ]);
+  const [quests] = useState([
+    "Парень лежит на спине. Девушка медленно проводить грудью с ног до головы по парню, а в конце по очереди дает парню целовать два сосочка",
+    "Парень полторы (две) минуты ласкает, целует ступни девушки",
+    "Парень две минуты облизывает, кусает, целует попку девушки",
+    "Пара подходит к зеркалу и целуется на протяжении ~1 минуты",
+    "Взять девушку двумя руками за грудь, и 10 раз проникнуть языком в вагину",
+    "Девушка на протяжении 30 секунд водит грудью по спине парня",
+    "Девушка садится на стул и закидывает ноги на мужчину. Мужчина 2 минуты делает куни",
+    "Парень лежит на спине. Девушка медленно проводить грудью с ног до головы по парню, а в конце по очереди дает парню целовать два сосочка.",
+    "Парень ложится на спину. Девушка водит по лицу парня своей вагиной как захочет на протяжении 2 минут",
+    "Девушка целует парня от губ до члена на протяжении 1 минуты",
+    "Девушка прогибает спину и пытается полностью покласть грудь на кровать. Мужчина вводит палец или пальцы в девушку и минуту делает круговые движения внутри",
+    "Девушка ложится на край кровати. Парень садится на колени перед кроватью. максимально широко раздвигает ноги девушки в шпагат и делает куни на протяжении полторы минуты"
+  ]);
 
-    const [chosenTime, setChosenTime] = useState(0);
-    const [chosenPlace, setChosenPlace] = useState("Hot");
-    const [chosenAction, setChosenAction] = useState("Hot");
+  const [chosenTime, setChosenTime] = useState(0);
+  const [chosenPlace, setChosenPlace] = useState("Hot");
+  const [chosenAction, setChosenAction] = useState("Hot");
 
-    const [chosenTimeSecond, setChosenTimeSecond] = useState(0);
-    const [chosenPlaceSecond, setChosenPlaceSecond] = useState("Hot");
-    const [chosenActionSecond, setChosenActionSecond] = useState("Hot");
+  const [chosenTimeSecond, setChosenTimeSecond] = useState(0);
+  const [chosenPlaceSecond, setChosenPlaceSecond] = useState("Hot");
+  const [chosenActionSecond, setChosenActionSecond] = useState("Hot");
 
-    const [chosenQuest, setChosenQuest] = useState("Нажми на кнопку");
-    const [increment, setIncrement] = useState(0);
+  const [chosenQuest, setChosenQuest] = useState("Нажми на кнопку");
+  const [increment, setIncrement] = useState(0);
 
-    const playFirst = () => {
-        setChosenTime(times[Math.floor(Math.random() * times.length)]);
-        setChosenPlace(places[Math.floor(Math.random() * places.length)]);
-        setChosenAction(actions[Math.floor(Math.random() * actions.length)]);
-    }
+  const playFirst = () => {
+    setChosenTime(times[Math.floor(Math.random() * times.length)]);
+    setChosenPlace(places[Math.floor(Math.random() * places.length)]);
+    setChosenAction(actions[Math.floor(Math.random() * actions.length)]);
+  };
 
-    const playSecond = () => {
-        setChosenTimeSecond(times[Math.floor(Math.random() * times.length)]);
-        setChosenPlaceSecond(placesTwo[Math.floor(Math.random() * places.length)]);
-        setChosenActionSecond(actionsTwo[Math.floor(Math.random() * actions.length)]);
-    }
-
-    const playQuest = () => {
-        if ( increment >= quests.length-1 ) {
-            setIncrement(0);
-            setChosenQuest(quests[increment])
-        } else {
-            setChosenQuest(quests[increment])
-            setIncrement(increment + 1);
-        }
-    }
-    return (
-        <>
-            <HashRouter>
-                <Route exact path="/">
-                    <div style={{marginTop: "-17px"}} className="container">
-                        <Time chosenTime={chosenTime}/>
-                        <div className={"main-container"}>
-                            <Place chosenPlace={chosenPlace}/>
-
-                            <Action chosenAction={chosenAction}/>
-                        </div>
-                        <Button play={playFirst}/>
-                    </div>
-
-                    <div style={{borderTop: "1px solid #0000002e", paddingTop: '50px', marginTop: "30px", marginBottom: "0px"}}
-                         className="container">
-                        <Time chosenTime={chosenTimeSecond}/>
-                        <div className={"main-container"}>
-                            <Place chosenPlace={chosenPlaceSecond}/>
-                            <Action chosenAction={chosenActionSecond}/>
-                        </div>
-                        <Button play={playSecond}/>
-                        <ModeButton/>
-                    </div>
-                </Route>
-                <Route path="/quests">
-                    <Quests playQuest={playQuest} quest={chosenQuest}/>
-                </Route>
-            </HashRouter>
-        </>
+  const playSecond = () => {
+    setChosenTimeSecond(times[Math.floor(Math.random() * times.length)]);
+    setChosenPlaceSecond(placesTwo[Math.floor(Math.random() * places.length)]);
+    setChosenActionSecond(
+      actionsTwo[Math.floor(Math.random() * actions.length)]
     );
+  };
+
+  const [isBackground, setIsBackground] = useState(false);
+
+  const playQuest = () => {
+    if (increment >= quests.length - 1) {
+      setIncrement(0);
+      setChosenQuest(quests[increment]);
+    } else {
+      setChosenQuest(quests[increment]);
+      setIncrement(increment + 1);
+    }
+  };
+  return (
+    <>
+      <HashRouter>
+        <Route exact path="/">
+          <div className={isBackground ? "with-background" : null}>
+            <div style={{ marginTop: "-17px" }} className={"container"}>
+              <Time chosenTime={chosenTime} />
+              <div className={"main-container"}>
+                <Place chosenPlace={chosenPlace} />
+
+                <Action chosenAction={chosenAction} />
+              </div>
+              <Button play={playFirst} />
+            </div>
+
+            <div
+              style={{
+                borderTop: "1px solid #0000002e",
+                paddingTop: "50px",
+                marginBottom: "0px"
+              }}
+              className={
+                isBackground ? "container second-with-bg" : "container second"
+              }
+            >
+              <Time chosenTime={chosenTimeSecond} />
+              <div className={"main-container"}>
+                <Place chosenPlace={chosenPlaceSecond} />
+                <Action chosenAction={chosenActionSecond} />
+              </div>
+              <Button play={playSecond} />
+              <ModeButton />
+              <div className={"bg-button-container"}>
+                <span
+                  className={"bg-button"}
+                  onClick={() => setIsBackground(!isBackground)}
+                >
+                  Background
+                </span>
+              </div>
+            </div>
+          </div>
+        </Route>
+        <Route path="/quests">
+          <Quests playQuest={playQuest} quest={chosenQuest} />
+        </Route>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -4,7 +4,7 @@ import { ReactComponent as Logo} from '../Infinity-1s-200px.svg';
 export const Preloader = () => {
     return (
         <div>
-            <Logo style={{backgroundColor: "#272727", height: "50px", width: "50px", color: "#fff" }}/>
+            <Logo style={{backgroundColor: "#272727", height: "30px", width: "30px", color: "#fff" }}/>
         </div>
     );
 };
